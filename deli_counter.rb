8ce5f134043a77709d8b0bb8fc521ece8f27fcb8
Caroline Berg <carolineeberg@gmail.com>
2018-08-05 23:23:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def line(customers)
+  array_new = []
+  customers_in_line = customers.size
+  assigned_customers = 0
+    if customers.count == 0
+      puts "The line is currently empty."
+    else
+      while assigned_customers < customers_in_line
+      announcement = "#{assigned_customers + 1}. #{customers[assigned_customers]}"
+      assigned_customers += 1
+      array_new.push(announcement)
+      end
+      final = array_new.join(" ")
+      puts "The line is currently: #{final}"
+    end
+end
+
+def take_a_number(customers, new_customer)
+  customers.push(new_customer)
+  assigned_customers = customers.count
+  return "#{customers.last} is #{assigned_customers} in line"
+end
